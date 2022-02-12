3c07c56332c5ad10aee8efe048d98c012cd9e1a7
Jason Serafica <seraficaj@gmail.com>
2022-02-11 21:29:40 -0800

    solve areThereDuplicates

--- OptionalCodingChallenges/FrequencyCounters.js
+++ OptionalCodingChallenges/FrequencyCounters.js
@@ -18,8 +18,23 @@ function sameFrequency(num1, num2) {
     }
     return true;
 }
-console.log(sameFrequency(3589578, 5879385));
-console.log(sameFrequency(22, 222));
+// console.log(sameFrequency(3589578, 5879385));
+// console.log(sameFrequency(22, 222));
 
 
 // 2. Write a func called areThereDuplicates which accepts a variable number of arguments, and checks whether there are any dupes among the args passed in. You can solve this problem using the frequency counter pattern or the multiple pointers pattern.
+
+function areThereDuplicates() {
+    let arr = [...arguments];
+    let container = {};
+    for (let index in arr) {
+        container[arr[index]] = (container[arr[index]] || 0) + 1;
+        console.log(index);
+        console.log(container);
+        if (container[arr[index]] > 1) return true;
+    }
+    return false;
+};
+
+console.log(areThereDuplicates(1,2,3)) // false
+console.log(areThereDuplicates(1,2,2)) // true
