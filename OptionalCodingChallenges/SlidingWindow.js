0db813df22e9404f100216ea44bcf2ddbe9f3c24
Jason Serafica <seraficaj@gmail.com>
2022-02-19 17:08:18 -0800

    Begin Sliding Window problem 1

---
+++ OptionalCodingChallenges/SlidingWindow.js
@@ -0,0 +1,11 @@
+// 1. Given an arr of integers and a num, write a function called MaxArraySum which finds the max sum of a subarray with the length of the number passed to the function.
+
+// Note that a subarray must consist of consecutive elements from the original array. In the first example below, [100, 200, 300] is a subarray of the original, but [100, 300] is not.
+
+function maxSubarraySum(arr, num) {
+    
+}
+
+maxSubarraySum([100, 200, 300,400], 2) // 700
+maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4) // 39
+maxSubarraySum([2,3], 3) // null
