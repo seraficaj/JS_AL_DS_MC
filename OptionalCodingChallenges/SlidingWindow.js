792a7b08aad6c9558cc96a0f9a13cbcb62941f3d
Jason Serafica <seraficaj@gmail.com>
2022-03-02 20:13:05 -0800

    add minsubarraylen solution

--- OptionalCodingChallenges/SlidingWindow.js
+++ OptionalCodingChallenges/SlidingWindow.js
@@ -2,35 +2,56 @@
 
 // Note that a subarray must consist of consecutive elements from the original array. In the first example below, [100, 200, 300] is a subarray of the original, but [100, 300] is not.
 
-const maxSubarraySum = (arr,num) => {
+const maxSubarraySum = (arr, num) => {
     let maxSum = 0;
     let tempSum = 0;
     if (arr.length < num) return null;
-    for (let i=0; i < num; i++) {
+    for (let i = 0; i < num; i++) {
         maxSum += arr[i];
     }
     tempSum = maxSum;
     for (let i = num; i < arr.length; i++) {
-        tempSum = tempSum - arr[i-num] + arr[i];
+        tempSum = tempSum - arr[i - num] + arr[i];
         maxSum = Math.max(maxSum, tempSum);
     }
     return maxSum;
-}
-maxSubarraySum([100, 200, 300,400], 2) // 700
-maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4) // 39
-maxSubarraySum([2,3], 3) // null
+};
+maxSubarraySum([100, 200, 300, 400], 2); // 700
+maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4); // 39
+maxSubarraySum([2, 3], 3); // null
 
 // 2. Given an arr of integers and a num, write a function called minSubArrayLen which finds the min length of a continguous subarray with the sum >= the number passed to the function. If there isn't one, return 0 instead.
 
 // Note that a subarray must consist of consecutive elements from the original array. In the first example below, [100, 200, 300] is a subarray of the original, but [100, 300] is not.
 
 function minSubArrayLen(arr, num) {
-    let minLength = 0;
-    let tempSum = 0;
-    while (tempSum < num) {
-        
+    let total = 0;
+    let start = 0;
+    let end = 0;
+    let minLen = Infinity;
+
+    while (start < nums.length) {
+        // if current window doesn't add up to the given sum then
+        // move the window to right
+        if (total < sum && end < nums.length) {
+            total += nums[end];
+            end++;
+        }
+        // if current window adds up to at least the sum given then
+        // we can shrink the window
+        else if (total >= sum) {
+            minLen = Math.min(minLen, end - start);
+            total -= nums[start];
+            start++;
+        }
+        // current total less than required total but we reach the end, need this or else we'll be in an infinite loop
+        else {
+            break;
+        }
     }
+
+    return minLen === Infinity ? 0 : minLen;
 }
 
-minSubArrayLen([2,3,1,2,4,3], 7) // 2 because [4,3] is the smallest subarray
-minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19], 52) // 1 because [62] is greater than 52
+minSubArrayLen([2, 3, 1, 2, 4, 3], 7); // 2 because [4,3] is the smallest subarray
+minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52); // 1 because [62] is greater than 52
