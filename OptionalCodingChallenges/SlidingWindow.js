b2fff9f77a05ce1e095e78575419c3e500a80406
Jason Serafica <seraficaj@gmail.com>
2022-03-06 15:56:44 -0800

    Sliding Window Problem: findLongestSubstring complete

--- OptionalCodingChallenges/SlidingWindow.js
+++ OptionalCodingChallenges/SlidingWindow.js
@@ -25,24 +25,51 @@ maxSubarraySum([2, 3], 3); // null
 // Note that a subarray must consist of consecutive elements from the original array. In the first example below, [100, 200, 300] is a subarray of the original, but [100, 300] is not.
 
 function minSubArrayLen(arr, num) {
-   let result = Infinity;
-   // set up window
-   let left = 0;
-   let sum = 0;
+    // largest possible array size
+    let result = Infinity;
+    // set up window
+    let left = 0;
+    let sum = 0;
 
-   for (let i =0 ; i <arr.length; i++) {
-       sum += arr[i];
-       while (sum >= num) {
-           // compare between current length 
-           result = Math.min(result, i - left + 1);
-           sum -= arr[left++];
-       }
-   }
-   if (result === Infinity) {
-       return 0
-   };
-   return result;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+        while (sum >= num) {
+            // compare between current length and window
+            result = Math.min(result, i - left + 1);
+            sum -= arr[left];
+            left++;
+        }
+    }
+    if (result === Infinity) {
+        return 0;
+    }
+    return result;
 }
 
 minSubArrayLen([2, 3, 1, 2, 4, 3], 7); // 2 because [4,3] is the smallest subarray
 minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52); // 1 because [62] is greater than 52
+
+/*
+Write a function called findLongestSubstring which accepts a str and returns length of the longest substr with all distinct characters.
+*/
+
+const findLongestSubstring = (str) => {
+    let start = 0;
+    let chars = {};
+    let longest = 0;
+
+    for (let i=0; i < str.length; i++) {
+        let char = str[i];
+        if (chars[char]) {
+            start = Math.max(start, chars[char]);
+        }
+
+        longest = Math.max(longest, i - start + 1);
+        chars[char] = i + 1;
+    }
+    return longest;
+}
+
+findLongestSubstring('') //0
+findLongestSubstring('rithmschool') //7
+findLongestSubstring('thisisawesome') //6
