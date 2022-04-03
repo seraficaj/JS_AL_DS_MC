0f3aa9fe6ede18863bdbe33ba423509323271f29
Jason Serafica <seraficaj@gmail.com>
2022-04-02 20:44:33 -0700

    begin 1st set of challenging recursion problems

---
+++ Recursion/11-ChallengingRecursionSet.js
@@ -0,0 +1,13 @@
+// 1. Return a new string in reverse.
+
+function reverse(){
+    // add whatever parameters you deem necessary - good luck!
+  }
+  
+  // reverse('awesome') // 'emosewa'
+
+  //2. Write a function which returns true if string passed to it is a palindrome. Otherwise, return false.
+
+  //3. Write a function called someRecursive which accepts an array and a callback. Function returns true if a single value in array returns true when passed to the callback. Otherwise, return false.
+
+  //4. Write a recursive function called flatten which accepts an array of arrays and returns a new array with all values flattened.
