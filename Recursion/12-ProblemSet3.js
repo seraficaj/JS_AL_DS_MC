b2a277d7448d97faedf30c8240bbb8402ebf486e
Jason Serafica <seraficaj@gmail.com>
2022-04-19 18:54:01 -0700

    3rd recursion problem set

---
+++ Recursion/12-ProblemSet3.js
@@ -0,0 +1,9 @@
+// make a function that capitalizes the first letter of each word in an array of strings
+
+// write a function that returns the sum of all even numbers in an object which may contain nested objects
+
+// write a function that takes in an array of words and returns a new array containing each word capitalized
+
+// write a function which takes in an object and fins all of the values which are numbers and converts them to strings
+
+// write a function called collectStrings which accepts an object and returns an array of all values in the object that have a typeof string
