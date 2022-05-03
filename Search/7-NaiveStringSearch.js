c952279fa5cb8442ea4786a1c80bf1e8983bd847
Jason Serafica <seraficaj@gmail.com>
2022-05-03 11:49:16 -0700

    code implementation of naive string search

---
+++ Search/7-NaiveStringSearch.js
@@ -0,0 +1,15 @@
+const stringSearch = (str, sub) => {
+    let count = 0;
+    // loop through str
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < sub.length; j++) {
+            if (sub[j] !== str[i+j]) {
+                break;
+            }
+            if (j === sub.length - 1) count++;
+        }
+    }
+    return count;
+};
+
+console.log(stringSearch("lorie loled lollapalooza", "lol"));
